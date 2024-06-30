92ac26816f9eab34931c860ce395f518d88a64fb
smonolo <lu3mtex@gmail.com>
2024-06-30 22:17:11 +0200

    feat(website): add resume card to home page

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+
+import classNames from 'classnames'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+  const [checked, setChecked] = useState<boolean>(false)
+
+  const linkProps = {
+    href: checked ? 'https://resume.smnl.dev' : '',
+    target: checked ? '_blank' : '_self',
+  }
+
   return (
     <section>
       <h1 className="text-3xl font-bold md:text-4xl">Hi there.</h1>
@@ -8,6 +19,35 @@ export default function Home() {
         I am a software engineer deeply passionate about the web and its
         technologies.
       </p>
+      <div className="from-sm-blue/70 to-sm-blue/50 mt-6 rounded-xl bg-gradient-to-b px-10 py-8">
+        <h3 className="text-2xl font-semibold">Looking for my resume?</h3>
+        <p className="mt-2">
+          You are (not) in the right place! I made a site for that, which
+          represents exactly how the PDF version looks but with a bit of
+          responsiveness for those who are always on the phone.
+        </p>
+        <div className="mt-4 flex items-center gap-x-2">
+          <input
+            type="checkbox"
+            onChange={(event) => setChecked(event.target.checked)}
+          />
+          <p className="text-sm text-neutral-300">
+            I understand that the resume does not have a dark theme, and that I
+            have prepared my eyes for the light.
+          </p>
+        </div>
+        <Link {...linkProps} className="mt-4 block w-fit">
+          <button
+            className={classNames(
+              'bg-sm-black text-sm-whit rounded-lg px-4 py-2 font-medium transition-colors disabled:opacity-50',
+              { 'hover:bg-sm-white hover:text-sm-black': checked }
+            )}
+            disabled={!checked}
+          >
+            Take me there
+          </button>
+        </Link>
+      </div>
       <p className="mt-6 text-sm text-neutral-300 md:text-base">
         Want to know more? Take a look at my{' '}
         <Link
--- apps/website/components/layout/navbar.tsx
+++ apps/website/components/layout/navbar.tsx
@@ -9,41 +9,46 @@ export default function Navbar() {
   const pathname = usePathname()
 
   return (
-    <nav className="fixed left-0 top-0 grid w-full grid-cols-3 border-b border-solid border-neutral-800 px-5 drop-shadow backdrop-blur-md lg:w-full">
-      <div className="flex w-full items-center justify-center py-3">
-        <Link href="/" className="flex w-fit items-center gap-x-2">
-          <div className="bg-sm-blue h-[20px] w-[20px] rounded-sm md:h-[25px] md:w-[25px]" />
-          <span className="text-sm-white text-sm font-medium md:text-base">
-            Stefano Monolo
-          </span>
-        </Link>
-      </div>
-      <div className="flex w-full items-center justify-center gap-x-5 border-x border-dashed border-neutral-800 py-3">
-        {Object.entries(data.items).map(([key, value]) => (
-          <Link
-            key={key}
-            href={value.url}
-            className={classNames(
-              'hover:text-sm-white text-sm text-neutral-300 transition-colors md:text-base',
-              { 'text-sm-white': pathname === value.url }
-            )}
-          >
-            {value.text}
+    <nav className="fixed left-0 top-0 w-full border-b border-solid border-neutral-800 px-5 drop-shadow backdrop-blur-md lg:w-full">
+      <div className="mx-auto flex max-w-[1000px] items-center justify-between py-4">
+        <div className="flex w-full items-center">
+          <Link href="/" className="flex w-fit items-center gap-x-2">
+            <div className="bg-sm-blue h-[20px] w-[20px] rounded-sm md:h-[25px] md:w-[25px]" />
+            <span className="text-sm-white text-sm font-medium md:text-base">
+              Stefano Monolo
+            </span>
           </Link>
-        ))}
-      </div>
-      <div className="flex w-full items-center justify-center gap-x-3 py-3">
-        {Object.entries(data.socials).map(([icon, url]) => (
-          <Link
-            key={icon}
-            href={url}
-            target={url.startsWith('https://') ? '_blank' : '_self'}
-          >
-            <i
-              className={`bi bi-${icon} hover:text-sm-white text-sm text-neutral-300 transition-colors`}
-            />
-          </Link>
-        ))}
+        </div>
+        <div className="flex w-full items-center justify-end gap-x-4">
+          <div className="flex w-fit items-center gap-x-3">
+            {Object.entries(data.socials).map(([icon, url]) => (
+              <Link
+                key={icon}
+                href={url}
+                target={url.startsWith('https://') ? '_blank' : '_self'}
+              >
+                <i
+                  className={`bi bi-${icon} hover:text-sm-white text-sm text-neutral-300 transition-colors`}
+                />
+              </Link>
+            ))}
+          </div>
+          <div className="h-[20px] w-[1px] bg-neutral-800" />
+          <div className="flex w-fit items-center gap-x-3">
+            {Object.entries(data.items).map(([key, value]) => (
+              <Link
+                key={key}
+                href={value.url}
+                className={classNames(
+                  'hover:text-sm-white text-sm text-neutral-300 transition-colors md:text-base',
+                  { 'text-sm-white': pathname === value.url }
+                )}
+              >
+                {value.text}
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </nav>
   )
