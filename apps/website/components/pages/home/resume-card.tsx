8af7fdd9d3052baa0e28ce4a8b1f7fef394b972c
smonolo <lu3mtex@gmail.com>
2024-08-18 17:26:09 +0200

    feat(website): improve resume card

--- apps/website/components/pages/home/resume-card.tsx
+++ apps/website/components/pages/home/resume-card.tsx
@@ -13,33 +13,40 @@ export default function ResumeCard() {
   }, [isChecked])
 
   return (
-    <div className="from-sm-blue/70 to-sm-blue/50 mt-8 rounded-2xl bg-gradient-to-b p-10">
+    <div className="from-sm-blue/70 to-sm-blue/60 mt-8 rounded-2xl bg-gradient-to-b px-10 py-8 md:rounded-3xl md:px-14 md:py-12">
       <h3 className="text-2xl font-semibold">Looking for my resume?</h3>
       <p className="mt-3">
         You are (not) in the right place! I made a site for that, which
-        represents exactly how the PDF version looks but with a bit of
-        responsiveness for those who are always on the phone.
+        represents how the PDF version looks but with a bit of responsiveness
+        for those who are always on the phone.
       </p>
       <div className="mt-5 flex items-center gap-x-2">
         <input
+          id="resume-checkbox"
           type="checkbox"
           onChange={(event) => setChecked(event.target.checked)}
         />
-        <p className="text-sm text-neutral-300">
+        <label htmlFor="resume-checkbox" className="text-sm text-neutral-300">
           I understand that the resume does not have a dark theme, and that I
           have prepared my eyes for the light.
-        </p>
+        </label>
       </div>
       <button
         className={classNames(
-          'bg-sm-black text-sm-white disabled:text-sm-white/50 disabled:bg-sm-black/50 z-10 mt-5 rounded-lg px-4 py-2 font-medium transition-colors',
-          { 'hover:bg-sm-white hover:text-sm-black': isChecked },
+          'bg-sm-black text-sm-white disabled:text-sm-white/50 disabled:bg-sm-black/50 group z-10 mt-5 flex items-center gap-x-2 rounded-lg px-4 py-2 font-medium transition-colors',
+          { 'hover:bg-sm-black/80': isChecked },
           isChecked ? 'cursor-pointer' : 'cursor-not-allowed'
         )}
         disabled={!isChecked}
         onClick={goToResume}
       >
-        Take me there
+        <span>Take me there</span>
+        <i
+          className={classNames(
+            'bi bi-arrow-right relative transition-transform',
+            { 'group-hover:translate-x-1': isChecked }
+          )}
+        />
       </button>
     </div>
   )
