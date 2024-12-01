4ebef103a1cccac0a449f763f923896e6b8b24dd
smonolo <lu3mtex@gmail.com>
2024-12-01 14:44:15 +0100

    feat(website): remove vat number from footer

--- apps/website/components/layout/footer.tsx
+++ apps/website/components/layout/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mx-auto flex w-full max-w-[1000px] flex-col items-center justify-between gap-x-5 gap-y-1 border-t border-neutral-800 px-5 py-4 md:flex-row lg:px-0">
       <span className="text-sm text-neutral-300">
-        © {new Date().getFullYear()} Stefano Monolo. P.IVA 11729910965
+        © {new Date().getFullYear()} Stefano Monolo.
       </span>
       <Link
         href="https://git.new/monorepo"
