94b42a486db7b4b547eecc963535c3ad439c1935
smonolo <lu3mtex@gmail.com>
2024-12-05 16:17:29 +0100

    feat(website): always use mobile navbar

--- apps/website/components/layout/navbar.tsx
+++ apps/website/components/layout/navbar.tsx
@@ -23,56 +23,60 @@ export default function Navbar() {
         </div>
         <div
           className={classNames(
-            'bg-sm-black/80 absolute left-0 top-0 z-50 h-screen w-full md:relative md:block md:h-fit md:bg-transparent',
+            'bg-sm-black/80 absolute left-0 top-0 z-50 h-screen w-full',
             { hidden: !isMenuOpen }
           )}
           onClick={() => setMenuOpen(false)}
         >
           <div
-            className="bg-sm-black absolute left-0 top-0 flex w-full flex-col-reverse gap-x-5 gap-y-4 border-b border-neutral-800 px-5 py-8 md:relative md:flex-row md:items-center md:justify-end md:border-none md:bg-transparent md:p-0"
+            className="bg-sm-black absolute left-0 top-0 w-full border-b border-neutral-800"
             onClick={event => event.stopPropagation()}
           >
-            <ul className="flex w-fit items-center gap-x-3">
-              {Object.entries(data.socials).map(([icon, url]) => (
-                <li key={icon}>
-                  <Link
-                    href={url}
-                    target={url.startsWith('https://') ? '_blank' : '_self'}
-                    onClick={() => setMenuOpen(false)}
-                  >
-                    <i
-                      className={`bi bi-${icon} hover:text-sm-white text-neutral-300 transition-colors`}
-                    />
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            <div className="hidden h-6 w-px bg-neutral-800 md:block" />
-            <ul className="flex w-fit items-center gap-x-5">
-              {Object.entries(data.items).map(([key, value]) => (
-                <li key={key}>
-                  <Link
-                    href={value.url}
-                    className={classNames(
-                      'hover:text-sm-white text-sm font-medium text-neutral-300 underline-offset-2 transition-colors hover:underline md:text-base',
-                      { 'text-sm-white': pathname === value.url }
-                    )}
-                    onClick={() => setMenuOpen(false)}
-                  >
-                    {value.text}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <div className="mx-auto max-w-[1000px] px-5 py-8 md:px-0">
+              <ul className="flex w-fit flex-col gap-2">
+                {Object.entries(data.items).map(([key, value]) => (
+                  <li key={key}>
+                    <Link
+                      href={value.url}
+                      className={classNames(
+                        'hover:text-sm-white text-xl font-medium text-neutral-300 underline-offset-2 transition-colors hover:underline',
+                        { 'text-sm-white': pathname === value.url }
+                      )}
+                      onClick={() => setMenuOpen(false)}
+                    >
+                      {value.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-        <div
-          className="flex cursor-pointer flex-col gap-y-1 md:hidden"
-          onClick={() => setMenuOpen(true)}
-        >
-          {Array.from({ length: 3 }).map((_, index) => (
-            <div key={index} className="bg-sm-white h-px w-5" />
-          ))}
+        <div className="flex w-fit items-center gap-x-3 md:gap-x-5">
+          <ul className="flex w-fit items-center gap-x-3">
+            {Object.entries(data.socials).map(([icon, url]) => (
+              <li key={icon}>
+                <Link
+                  href={url}
+                  target={url.startsWith('https://') ? '_blank' : '_self'}
+                  onClick={() => setMenuOpen(false)}
+                >
+                  <i
+                    className={`bi bi-${icon} hover:text-sm-white text-neutral-300 transition-colors`}
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="h-6 w-px bg-neutral-800" />
+          <div
+            className="flex cursor-pointer flex-col gap-y-1 py-2"
+            onClick={() => setMenuOpen(true)}
+          >
+            {Array.from({ length: 3 }).map((_, index) => (
+              <div key={index} className="bg-sm-white h-px w-6" />
+            ))}
+          </div>
         </div>
       </div>
     </nav>
