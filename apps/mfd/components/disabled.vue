2962e5fad14c598d04efeee1ac7608bbd1dd4fcd
smonolo <lu3mtex@gmail.com>
2025-01-22 15:53:15 +0100

    feat(mfd): add mobile disabled screen

---
+++ apps/mfd/components/disabled.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineComponent({ name: 'MobileDisabled' })
+</script>
+
+<template>
+  <div
+    class="absolute z-50 flex h-screen w-full items-center justify-center bg-black text-white xl:hidden"
+  >
+    <div class="flex w-fit flex-col gap-y-2">
+      <h1 class="text-2xl font-bold">Disabled on mobile</h1>
+      <p>
+        I'm sorry, but this site is disable for devices with smaller displays.
+        Please use a tablet, laptop or desktop to view.
+      </p>
+    </div>
+  </div>
+</template>
--- apps/mfd/layouts/default.vue
+++ apps/mfd/layouts/default.vue
@@ -4,6 +4,7 @@ import Screen from '~/components/screen/index.vue'
 import { useButtons } from '~/composables/useButtons'
 import { useScreens } from '~/composables/useScreens'
 import { useScreenStore } from '~/stores/screen'
+import MobileDisabled from '~/components/disabled.vue'
 
 const { upperButtons, lowerButtons, sideButtons } = useButtons()
 const { screensConfig } = useScreens()
@@ -11,26 +12,29 @@ const screenStore = useScreenStore()
 </script>
 
 <template>
-  <div
-    class="flex h-screen select-none items-center justify-center bg-black text-white"
-  >
-    <div class="flex h-[900px] w-[1100px]">
-      <div class="flex h-full w-[900px] flex-col justify-between">
-        <section class="flex h-[100px] items-center justify-center">
-          <Buttons :buttons="upperButtons" />
-        </section>
-        <main class="h-[680px] bg-slate-900">
-          <Screen>
-            <component :is="screensConfig[screenStore.activeScreen]" />
-          </Screen>
-        </main>
-        <section class="flex h-[100px] items-center justify-center">
-          <Buttons type="controls" :buttons="lowerButtons" />
-        </section>
+  <div class="relative">
+    <MobileDisabled />
+    <div
+      class="flex h-screen select-none items-center justify-center bg-black text-white"
+    >
+      <div class="flex h-[900px] w-[1100px]">
+        <div class="flex h-full w-[900px] flex-col justify-between">
+          <section class="flex h-[100px] items-center justify-center">
+            <Buttons :buttons="upperButtons" />
+          </section>
+          <main class="h-[680px] bg-slate-900">
+            <Screen>
+              <component :is="screensConfig[screenStore.activeScreen]" />
+            </Screen>
+          </main>
+          <section class="flex h-[100px] items-center justify-center">
+            <Buttons type="controls" :buttons="lowerButtons" />
+          </section>
+        </div>
+        <aside class="flex w-[200px]">
+          <Buttons direction="vertical" :buttons="sideButtons" />
+        </aside>
       </div>
-      <aside class="flex w-[200px]">
-        <Buttons direction="vertical" :buttons="sideButtons" />
-      </aside>
     </div>
   </div>
 </template>
