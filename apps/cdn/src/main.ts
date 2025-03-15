fc5d9d26954d94c17b1b9804371bf9c52f378249
smonolo <lu3mtex@gmail.com>
2025-03-15 18:22:41 +0100

    feat(cdn): enable cors and allow get and post requests

--- apps/cdn/src/main.ts
+++ apps/cdn/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: ['OPTIONS', 'GET', 'POST'],
+    allowedHeaders: '*',
+  }
+
+  app.enableCors(corsOptions)
 
   await app.listen(49160)
 }
