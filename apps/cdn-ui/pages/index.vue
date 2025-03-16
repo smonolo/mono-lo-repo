9243c2b9df99b59165b868c25fc2166bef91f2e0
smonolo <lu3mtex@gmail.com>
2025-03-16 02:50:09 +0100

    feat(cdn-ui): add pagination

--- apps/cdn-ui/pages/index.vue
+++ apps/cdn-ui/pages/index.vue
@@ -11,43 +11,68 @@ type File = {
 type Response = {
   files: File[]
   totalSize: number
+  totalFiles: number
+  totalPages: number
 }
 
 const route = useRoute()
 
-const response = await fetch(
-  `https://cdn.smnl.it/list?authKey=${route.query.authKey}`
-)
-const data: Response = await response.json()
+const page = ref(1)
+const data = ref<Response | null>(null)
+
+const fetchData = async () => {
+  const response = await fetch(
+    `https://cdn.smnl.it/list?authKey=${route.query.authKey}&page=${page.value}`
+  )
+  data.value = await response.json()
+}
+
+watch(page, fetchData, { immediate: true })
+
+const openFile = (fileName: string) => {
+  window.open(`https://cdn.smnl.it/${fileName}`)
+}
 </script>
 
 <template>
-  <table class="w-full">
-    <thead>
-      <tr>
-        <th>{{ data.files.length }}/{{ filesize(data.totalSize) }}</th>
-        <th>Name</th>
-        <th>Size</th>
-        <th>Upload Date</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(file, index) in data.files" :key="file.fileName">
-        <td>
-          {{ data.files.length - index }}
-        </td>
-        <td>
-          <a
-            :href="`https://cdn.smnl.it/${file.fileName}`"
-            target="_blank"
-            class="underline"
-          >
-            {{ file.fileName }}
-          </a>
-        </td>
-        <td>{{ filesize(file.size) }}</td>
-        <td>{{ dayjs(file.birthTime).format('DD/MM/YYYY HH:mm:ss') }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div v-if="!!data">
+    <table class="w-full">
+      <thead>
+        <tr>
+          <th>File ({{ data.totalFiles }})</th>
+          <th>Size ({{ filesize(data.totalSize) }})</th>
+          <th>Upload Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="file in data.files"
+          :key="file.fileName"
+          class="group cursor-pointer"
+          @click="openFile(file.fileName)"
+        >
+          <td>{{ file.fileName }}</td>
+          <td>{{ filesize(file.size) }}</td>
+          <td>{{ dayjs(file.birthTime).format('DD/MM/YYYY HH:mm:ss') }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="mt-4 flex w-fit border border-white">
+      <button
+        v-if="data.totalPages > 1 && page > 1"
+        class="cursor-pointer p-3 hover:bg-white hover:text-black"
+        @click="page--"
+      >
+        Previous
+      </button>
+      <span class="border-x border-white p-3">{{ page }}</span>
+      <button
+        v-if="data.totalPages > 1 && page < data.totalPages"
+        class="cursor-pointer p-3 hover:bg-white hover:text-black"
+        @click="page++"
+      >
+        Next
+      </button>
+    </div>
+  </div>
 </template>
