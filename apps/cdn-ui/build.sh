f3422bac433c61c17cec02aecadd0c874543db0a
smonolo <lu3mtex@gmail.com>
2025-03-16 02:53:12 +0100

    ci(cdn-ui): add build script

---
+++ apps/cdn-ui/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$VERCEL_ENV" != "production" ]; then
+    echo "Skipping build for non-production deployments"
+    exit 0
+fi
+
+echo "Detected production deployment, proceeding"
+
+npx turbo-ignore
