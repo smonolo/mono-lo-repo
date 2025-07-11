ea086f15a1689984eecb1b1f32f12d0ed0b0f71c
smonolo <lu3mtex@gmail.com>
2025-07-11 18:31:45 +0200

    feat(site): add horizontal padding to error page

--- apps/site/error.vue
+++ apps/site/error.vue
@@ -15,7 +15,7 @@ const showErrorDetails = ref(false)
 <template>
   <div>
     <NuxtLayout>
-      <div class="space-y-8">
+      <div class="space-y-8 md:px-[50px]">
         <div class="space-y-4">
           <a
             :href="`https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/${error.statusCode}`"
